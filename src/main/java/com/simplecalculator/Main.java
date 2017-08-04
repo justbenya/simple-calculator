7ebb8ebb544b490994fea86e9fd0bedfbf66ba14
Just Benya <justbenya@gmail.com>
2017-08-04 15:05:56 +0700

    Добавил mvn

---
+++ src/main/java/com/simplecalculator/Main.java
@@ -0,0 +1,14 @@
+package com.simplecalculator;
+
+/**
+ * Написать простой калькулятор для подсчета кол-во шантен
+ * Ввод через консоль в виде 123m123789p123s11z
+ * Проверять корректность ввода!
+ *
+ */
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
